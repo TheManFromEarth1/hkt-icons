df8115ace64cfb53d4bc52d98295fba3b04e3b82
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2022-06-20 20:14:42 +0000

    Add SolidJS support

---
+++ solid-js/index.js
@@ -0,0 +1,110 @@
+import h from "solid-js/h"
+
+export const App = () =>
+  h("img", { src: new URL("../default/App.svg", import.meta.url) })
+export const Art = () =>
+  h("img", { src: new URL("../default/Art.svg", import.meta.url) })
+export const BookmarkDefault = () =>
+  h("img", { src: new URL("../default/BookmarkDefault.svg", import.meta.url) })
+export const BookmarkSaved = () =>
+  h("img", { src: new URL("../default/BookmarkSaved.svg", import.meta.url) })
+export const Design = () =>
+  h("img", { src: new URL("../default/Design.svg", import.meta.url) })
+export const Dev = () =>
+  h("img", { src: new URL("../default/Dev.svg", import.meta.url) })
+export const Discord = () =>
+  h("img", { src: new URL("../default/Discord.svg", import.meta.url) })
+export const Drag = () =>
+  h("img", { src: new URL("../default/Drag.svg", import.meta.url) })
+export const Dropdown = () =>
+  h("img", { src: new URL("../default/Dropdown.svg", import.meta.url) })
+export const DownArrow = () =>
+  h("img", { src: new URL("../default/DownArrow.svg", import.meta.url) })
+export const Email = () =>
+  h("img", { src: new URL("../default/Email.svg", import.meta.url) })
+export const Europe = () =>
+  h("img", { src: new URL("../default/Europe.svg", import.meta.url) })
+export const Favorite = () =>
+  h("img", { src: new URL("../default/Favorite.svg", import.meta.url) })
+export const FolderFilled = () =>
+  h("img", { src: new URL("../default/FolderFilled.svg", import.meta.url) })
+export const Gaming = () =>
+  h("img", { src: new URL("../default/Gaming.svg", import.meta.url) })
+export const Home = () =>
+  h("img", { src: new URL("../default/Home.svg", import.meta.url) })
+export const Hookito = () =>
+  h("img", { src: new URL("../default/Hookito.svg", import.meta.url) })
+export const HookitoFull = () =>
+  h("img", { src: new URL("../default/HookitoFull.svg", import.meta.url) })
+export const Images = () =>
+  h("img", { src: new URL("../default/Images.svg", import.meta.url) })
+export const Insight = () =>
+  h("img", { src: new URL("../default/Insight.svg", import.meta.url) })
+export const Learn = () =>
+  h("img", { src: new URL("../default/Learn.svg", import.meta.url) })
+export const Liked = () =>
+  h("img", { src: new URL("../default/Liked.svg", import.meta.url) })
+export const Link = () =>
+  h("img", { src: new URL("../default/Link.svg", import.meta.url) })
+export const Live = () =>
+  h("img", { src: new URL("../default/Live.svg", import.meta.url) })
+export const Loading = () =>
+  h("img", { src: new URL("../default/Loading.svg", import.meta.url) })
+export const Lock = () =>
+  h("img", { src: new URL("../default/Lock.svg", import.meta.url) })
+export const Maps = () =>
+  h("img", { src: new URL("../default/Maps.svg", import.meta.url) })
+export const Marketing = () =>
+  h("img", { src: new URL("../default/Marketing.svg", import.meta.url) })
+export const Moon = () =>
+  h("img", { src: new URL("../default/Moon.svg", import.meta.url) })
+export const Move = () =>
+  h("img", { src: new URL("../default/Move.svg", import.meta.url) })
+export const Music = () =>
+  h("img", { src: new URL("../default/Music.svg", import.meta.url) })
+export const News = () =>
+  h("img", { src: new URL("../default/News.svg", import.meta.url) })
+export const Next = () =>
+  h("img", { src: new URL("../default/Next.svg", import.meta.url) })
+export { Pause } from "./Pause.js"
+export { Play } from "./Play.js"
+export const Podcast = () =>
+  h("img", { src: new URL("../default/Podcast.svg", import.meta.url) })
+export const Present = () =>
+  h("img", { src: new URL("../default/Present.svg", import.meta.url) })
+export const Previous = () =>
+  h("img", { src: new URL("../default/Previous.svg", import.meta.url) })
+export const Question = () =>
+  h("img", { src: new URL("../default/Question.svg", import.meta.url) })
+export const Radio = () =>
+  h("img", { src: new URL("../default/Radio.svg", import.meta.url) })
+export const Rain = () =>
+  h("img", { src: new URL("../default/Rain.svg", import.meta.url) })
+export const RSS = () =>
+  h("img", { src: new URL("../default/RSS.svg", import.meta.url) })
+export const Search = () =>
+  h("img", { src: new URL("../default/Search.svg", import.meta.url) })
+export const Seat = () =>
+  h("img", { src: new URL("../default/Seat.svg", import.meta.url) })
+export const Share = () =>
+  h("img", { src: new URL("../default/Share.svg", import.meta.url) })
+export const Shopping = () =>
+  h("img", { src: new URL("../default/Shopping.svg", import.meta.url) })
+export const Startpage = () =>
+  h("img", { src: new URL("../default/Startpage.svg", import.meta.url) })
+export const Stocks = () =>
+  h("img", { src: new URL("../default/Stocks.svg", import.meta.url) })
+export const Sun = () =>
+  h("img", { src: new URL("../default/Sun.svg", import.meta.url) })
+export const Throw = () =>
+  h("img", { src: new URL("../default/Throw.svg", import.meta.url) })
+export const Tree = () =>
+  h("img", { src: new URL("../default/Tree.svg", import.meta.url) })
+export const Unlock = () =>
+  h("img", { src: new URL("../default/Unlock.svg", import.meta.url) })
+export const Uparrow = () =>
+  h("img", { src: new URL("../default/Uparrow.svg", import.meta.url) })
+export const Volume = () =>
+  h("img", { src: new URL("../default/Volume.svg", import.meta.url) })
+export const WWW = () =>
+  h("img", { src: new URL("../default/WWW.svg", import.meta.url) })
