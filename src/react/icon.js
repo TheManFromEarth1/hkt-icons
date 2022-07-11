2a6e748dd91ccb95032e14d5deb6a88efd53108a
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2022-07-10 23:23:47 -0300

    Support older versions of TypeScript

--- src/react/icon.js
+++ src/react/icon.js
@@ -1,19 +1,14 @@
 import { createElement } from "react"
 
 const icon = (content) => {
-  return ({
-    fill = "currentColor",
-    width = 24,
-    height = 24,
-    ...rest
-  }) => {
+  return ({ fill = "currentColor", width = 24, height = 24, ...rest }) => {
     return createElement("svg", {
       dangerouslySetInnerHTML: { __html: content },
       xmlns: "http://www.w3.org/2000/svg",
       fill: fill,
       width: width,
       height: height,
-      ...rest
+      ...rest,
     })
   }
 }
--- src/solid/icon.js
+++ src/solid/icon.js
@@ -3,13 +3,16 @@ import h from "solid-js/h"
 
 const icon = (content) => {
   return (baseProps) => {
-    const props = mergeProps({
-      innerHTML: content,
-      fill: "currentColor",
-      width: 40,
-      height: 40,
-      xmlns: "http://www.w3.org/2000/svg",
-    }, baseProps)
+    const props = mergeProps(
+      {
+        innerHTML: content,
+        fill: "currentColor",
+        width: 40,
+        height: 40,
+        xmlns: "http://www.w3.org/2000/svg",
+      },
+      baseProps
+    )
 
     return h("svg", props)
   }
