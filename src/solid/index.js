db5075a90981960e517a33b7dfe9b94c512f7efa
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2022-07-15 12:54:59 -0300

    Add non-JSX version so we support both Astro and Parcel (#9)

---
+++ src/solid/index.js
@@ -0,0 +1,116 @@
+import icon from "./icon.js"
+
+import AppContent from "/icons/default/App.svg"
+export const App = icon(AppContent)
+import ArtContent from "/icons/default/Art.svg"
+export const Art = icon(ArtContent)
+import BookmarkDefaultContent from "/icons/default/BookmarkDefault.svg"
+export const BookmarkDefault = icon(BookmarkDefaultContent)
+import BookmarkSavedContent from "/icons/default/BookmarkSaved.svg"
+export const BookmarkSaved = icon(BookmarkSavedContent)
+import DesignContent from "/icons/default/Design.svg"
+export const Design = icon(DesignContent)
+import DevContent from "/icons/default/Dev.svg"
+export const Dev = icon(DevContent)
+import DiscordContent from "/icons/default/Discord.svg"
+export const Discord = icon(DiscordContent)
+import DownArrowContent from "/icons/default/DownArrow.svg"
+export const DownArrow = icon(DownArrowContent)
+import DragContent from "/icons/default/Drag.svg"
+export const Drag = icon(DragContent)
+import DropdownContent from "/icons/default/Dropdown.svg"
+export const Dropdown = icon(DropdownContent)
+import EmailContent from "/icons/default/Email.svg"
+export const Email = icon(EmailContent)
+import EuropeContent from "/icons/default/Europe.svg"
+export const Europe = icon(EuropeContent)
+import FavoriteContent from "/icons/default/Favorite.svg"
+export const Favorite = icon(FavoriteContent)
+import FolderFilledContent from "/icons/default/FolderFilled.svg"
+export const FolderFilled = icon(FolderFilledContent)
+import FolderUnfilledContent from "/icons/default/FolderUnfilled.svg"
+export const FolderUnfilled = icon(FolderUnfilledContent)
+import GamingContent from "/icons/default/Gaming.svg"
+export const Gaming = icon(GamingContent)
+import HomeContent from "/icons/default/Home.svg"
+export const Home = icon(HomeContent)
+import HookitoContent from "/icons/default/Hookito.svg"
+export const Hookito = icon(HookitoContent)
+import HookitoFullContent from "/icons/default/HookitoFull.svg"
+export const HookitoFull = icon(HookitoFullContent)
+import ImagesContent from "/icons/default/Images.svg"
+export const Images = icon(ImagesContent)
+import InsightContent from "/icons/default/Insight.svg"
+export const Insight = icon(InsightContent)
+import LearnContent from "/icons/default/Learn.svg"
+export const Learn = icon(LearnContent)
+import LikedContent from "/icons/default/Liked.svg"
+export const Liked = icon(LikedContent)
+import LinkContent from "/icons/default/Link.svg"
+export const Link = icon(LinkContent)
+import LiveContent from "/icons/default/Live.svg"
+export const Live = icon(LiveContent)
+import LoadingContent from "/icons/default/Loading.svg"
+export const Loading = icon(LoadingContent)
+import LockContent from "/icons/default/Lock.svg"
+export const Lock = icon(LockContent)
+import MapsContent from "/icons/default/Maps.svg"
+export const Maps = icon(MapsContent)
+import MarketingContent from "/icons/default/Marketing.svg"
+export const Marketing = icon(MarketingContent)
+import MoonContent from "/icons/default/Moon.svg"
+export const Moon = icon(MoonContent)
+import MoveContent from "/icons/default/Move.svg"
+export const Move = icon(MoveContent)
+import MusicContent from "/icons/default/Music.svg"
+export const Music = icon(MusicContent)
+import NewsContent from "/icons/default/News.svg"
+export const News = icon(NewsContent)
+import NextContent from "/icons/default/Next.svg"
+export const Next = icon(NextContent)
+import PauseContent from "/icons/default/Pause.svg"
+export const Pause = icon(PauseContent)
+import PlayContent from "/icons/default/Play.svg"
+export const Play = icon(PlayContent)
+import PodcastContent from "/icons/default/Podcast.svg"
+export const Podcast = icon(PodcastContent)
+import PresentContent from "/icons/default/Present.svg"
+export const Present = icon(PresentContent)
+import PreviousContent from "/icons/default/Previous.svg"
+export const Previous = icon(PreviousContent)
+import QuestionContent from "/icons/default/Question.svg"
+export const Question = icon(QuestionContent)
+import RadioContent from "/icons/default/Radio.svg"
+export const Radio = icon(RadioContent)
+import RainContent from "/icons/default/Rain.svg"
+export const Rain = icon(RainContent)
+import RSSContent from "/icons/default/RSS.svg"
+export const RSS = icon(RSSContent)
+import SearchContent from "/icons/default/Search.svg"
+export const Search = icon(SearchContent)
+import SeatContent from "/icons/default/Seat.svg"
+export const Seat = icon(SeatContent)
+import ShareContent from "/icons/default/Share.svg"
+export const Share = icon(ShareContent)
+import ShoppingContent from "/icons/default/Shopping.svg"
+export const Shopping = icon(ShoppingContent)
+import StartpageContent from "/icons/default/Startpage.svg"
+export const Startpage = icon(StartpageContent)
+import StocksContent from "/icons/default/Stocks.svg"
+export const Stocks = icon(StocksContent)
+import SunContent from "/icons/default/Sun.svg"
+export const Sun = icon(SunContent)
+import ThrowContent from "/icons/default/Throw.svg"
+export const Throw = icon(ThrowContent)
+import ThunderContent from "/icons/default/Thunder.svg"
+export const Thunder = icon(ThunderContent)
+import TreeContent from "/icons/default/Tree.svg"
+export const Tree = icon(TreeContent)
+import UnlockContent from "/icons/default/Unlock.svg"
+export const Unlock = icon(UnlockContent)
+import UparrowContent from "/icons/default/Uparrow.svg"
+export const Uparrow = icon(UparrowContent)
+import VolumeContent from "/icons/default/Volume.svg"
+export const Volume = icon(VolumeContent)
+import WWWContent from "/icons/default/WWW.svg"
+export const WWW = icon(WWWContent)
